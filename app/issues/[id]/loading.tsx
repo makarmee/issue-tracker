b73e7086c56341702407b6008bc322e03a30cc38
makarmee <mmahdikarimi2006@gmail.com>
2025-02-03 16:50:55 +0430

    additional loading skeletons added

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Flex, Card, Text, Box } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function LoadingIssueDetailPage() {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gapX="3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 }
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,7 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { prisma } from "@/prisma/client";
-import { Flex, Heading, Text, Card } from "@radix-ui/themes";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
 import ReactMarkdown from "react-markdown";
 
 interface Props {
